666ea52b5c8aec79077f73b27db2353a822c2905
Ramazonboy <ollanazarovramazon731@gmail.com>
2022-01-27 04:46:37 +0500

    first commidddt

--- js/function.js
+++ js/function.js
@@ -36,7 +36,7 @@ const sectionOne = () => {
     if (mediaQ.matches)
     {
         sectOneDiv.classList.remove('sectOne')
-        sectOneDiv.classList.add('my-3')
+        //sectOneDiv.classList.add('my-3')
         colRight.classList.add('my-3')
         colLeft.classList.add('my-3')
         //colLeft.classList.add('py-5')
